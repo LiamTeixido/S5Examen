59542c5a60fedc770bceecfa47d1ee32b1e971f1
LiamTeixido <liamteixido@gmail.com>
2023-09-29 00:18:07 -0500

    Corrección de Productos - Liam Teixido
    Se agregó el peso y el material

--- Prom1LiamTeixido/Prom1LiamTeixido/ProductClay.cs
+++ Prom1LiamTeixido/Prom1LiamTeixido/ProductClay.cs
@@ -6,32 +6,36 @@ using System.Threading.Tasks;
 
 namespace Prom1LiamTeixido
 {
-    class ProductClay: IProduct, IMaterial
+    public class ProductClay: Product, IMaterial
     {
         private string name;
         private int size;
         private string color;
         private int price;
+        private string material;
+        private int weight;
 
-        public ProductClay(string name, int size, string color, int price)
+        public ProductClay(string name, int size, string color, int price, string material, int weight)
         {
             this.name = name;
             this.size = size;
             this.color = color;
             this.price = price;
+            this.weight = weight;
+            this.material = material;
         }
 
-        public string GetFeatures()
+        public override string GetFeatures()
         {
-            return $"Nombre: {name}, Tamaño: {size}, Color: {color}, Precio: {price}";
+            return $"Nombre: {name}, Tamaño: {size}, Color: {color}, Precio: {price}, Peso: {weight}, Material: {material}";
         }
 
-        public string GetName()
+        public override string GetName()
         {
-            return "Tela";
+            return name;
         }
 
-        public int GetPrice()
+        public override int GetPrice()
         {
             return price;
         }
